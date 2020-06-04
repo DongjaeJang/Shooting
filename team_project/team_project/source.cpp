746ce8660ba072aa54075804e7164e12b2047463
DongjaeJang <jangdj001234@gmail.com>
2020-06-04 16:42:08 +0900

    team Project

---
+++ team_project/team_project/source.cpp
@@ -0,0 +1,540 @@
+#include <bangtal.h>
+
+//c standard
+#include <cstdio>
+#include <cstdlib>
+#include <ctime>
+
+//cpp standard
+#include <vector>
+#include <list>
+#include <queue>
+
+#define BACK_SCENE_FRAME_PER_PIXEL 5
+#define AGENT_UPDATE_TIME 0.01f
+#define OBJECT_UPDATE_TIME 0.01f
+#define AGENT_SPEED 20
+#define AGENT_HEIGHT 50
+#define AGENT_WIDTH 50
+#define ENEMY_1_GEN_RATE 0.5f
+#define ENEMY_1_HEIGHT 10
+#define ENEMY_1_WIDHT 10
+#define SCREEN_WIDTH 1280
+#define SCREEN_HEIGHT 760
+
+
+using namespace std;
+
+struct agent
+{
+	ObjectID obj;
+	int x = 0;
+	int y = 500;
+	int dx = 0, dy = 0;
+};
+
+struct movableObject //단순 이동객체 상호작용 x
+{
+	ObjectID obj;
+	int x;
+	int y;
+	int dx;
+	int dy;
+};
+
+struct enemyObject {
+	ObjectID obj;
+	int type;
+	int hp;
+	int x;
+	int y;
+	int dx;
+	int dy;
+};
+
+struct bulitObject{
+	ObjectID obj;
+	int damage;
+	int x;
+	int y;
+	int dx;
+	int dy;
+};
+
+
+
+/*
+Scenes
+*/
+SceneID backgroundScene;
+
+/*
+TimerID
+*/
+TimerID objectUpdateTimer;
+TimerID agentUpdateTimer;
+TimerID backgroundMovingTimer;
+TimerID enemy1GenTimer;
+
+/*
+ObjectIDs
+*/
+
+/*
+UI component
+*/
+ObjectID startButtonObj, endButtonObj;
+
+/*
+Other variables
+*/
+
+agent heroAgent;
+movableObject backSceneObject;
+
+
+// index를 활용하지 않고, iterator기반으로만 접근하므로, list가 좋음
+// 삽입속도는 상관 없지만 mid기준 제거속도가 list가 더빠름
+list<enemyObject> enemyList1; 
+list<bulitObject> bulitList;
+
+bool gameStarted = false;
+
+
+ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown);
+void keyboardCallback(KeyCode kc, KeyState ks);
+void timerCallback(TimerID timer);
+void mouseCallback(ObjectID object, int x, int y, MouseAction action);
+void endGame(bool success);
+void startGame();
+
+pair<bool,int> isBulitHit(int x, int y, int width, int height);
+bool isAgentHit(int x, int y, int width, int height);
+void initTimers();
+void initObjects();
+void initBackScene();
+void initHeroAgent();
+void initBulits();
+void initEnemys();
+void createEnemy(int type);
+void agentMove(int dx, int dy);
+void createBulit(int type);
+int main()
+{
+	srand((unsigned int)time(0)); // init random seed
+
+	setMouseCallback(mouseCallback);
+	setTimerCallback(timerCallback);
+	setKeyboardCallback(keyboardCallback);
+
+	/*
+	scenes
+	*/
+	backgroundScene = createScene("코딩하기 싫다", "Images/background.png");
+	backSceneObject.x = 0;
+	backSceneObject.y = -150;
+	backSceneObject.dx = BACK_SCENE_FRAME_PER_PIXEL;
+	backSceneObject.obj = createObject("Images/nebula03.png", backgroundScene, backSceneObject.x, backSceneObject.y, true);
+
+	/*
+	UI
+	*/
+	startButtonObj = createObject("Images/start.png", backgroundScene, 590, 70, true);
+	endButtonObj = createObject("Images/end.png", backgroundScene, 590, 20, true);
+	
+	/*
+	Objects
+	*/
+	heroAgent.obj = createObject("Images/agent.png", backgroundScene, heroAgent.x, heroAgent.y, true);
+	
+	
+	/*
+	Timers
+	*/
+	backgroundMovingTimer = createTimer(0.1f);
+	agentUpdateTimer = createTimer(AGENT_UPDATE_TIME);
+	objectUpdateTimer = createTimer(OBJECT_UPDATE_TIME);
+	enemy1GenTimer = createTimer(ENEMY_1_GEN_RATE);
+
+	startGame(backgroundScene);
+}
+/*
+
+callbacks
+
+*/
+
+void mouseCallback(ObjectID object, int x, int y, MouseAction action)
+{
+
+	if (object == endButtonObj) {
+		endGame();
+	}
+	else if (object == startButtonObj) {
+		startGame();
+	}
+
+}
+
+void timerCallback(TimerID timer)
+{
+	if (timer == agentUpdateTimer)
+	{
+		//모든 객체 update
+		if (gameStarted == false)
+			return;
+		agentMove(heroAgent.dx, heroAgent.dy);
+
+		setTimer(agentUpdateTimer, AGENT_UPDATE_TIME);
+		startTimer(agentUpdateTimer);
+
+	}
+	else if (timer ==objectUpdateTimer)
+	{
+
+		//미사일 위치 갱신
+		list<bulitObject>::iterator iterB;
+		for (iterB = bulitList.begin(); iterB != bulitList.end(); )
+		{
+			(*iterB).x += (*iterB).dx;
+			(*iterB).y += (*iterB).dy;
+
+			locateObject((*iterB).obj, backgroundScene, (*iterB).x, (*iterB).y);
+			if ((*iterB).x > SCREEN_WIDTH + 100)
+			{
+				iterB = bulitList.erase(iterB); // 범위를 벗어났을때 목록에서 제거
+			}
+			else
+			{
+				iterB++;
+			}
+		}
+		//적 위치 갱신
+		list<enemyObject>::iterator iterE;
+		for (iterE = enemyList1.begin(); iterE != enemyList1.end(); )
+		{
+			pair<bool, int> hit;
+			bool destroy=false;
+			(*iterE).x += (*iterE).dx;
+			(*iterE).y += (*iterE).dy;
+			locateObject((*iterE).obj, backgroundScene, (*iterE).x, (*iterE).y);
+
+
+			//총알과 부딫혔는지
+			hit = isBulitHit((*iterE).x, (*iterE).y, ENEMY_1_WIDHT, ENEMY_1_HEIGHT);
+			if (hit.first)
+			{
+				(*iterE).hp -= hit.second;
+				if ((*iterE).hp <= 0)
+					destroy = true;
+				
+			}
+			//조종대상에 부딫혔을때
+			if(isAgentHit((*iterE).x, (*iterE).y,AGENT_WIDTH,AGENT_HEIGHT))
+			{
+				endGame(false);
+				return;
+			}
+			else if ((*iterE).x < -100 || destroy)
+			{
+				hideObject((*iterE).obj);
+				iterE = enemyList1.erase(iterE); // 범위를 벗어났을때 목록에서 제거
+			}
+			else
+			{
+				iterE++;
+			}
+		}
+
+
+		setTimer(objectUpdateTimer, OBJECT_UPDATE_TIME);
+		startTimer(objectUpdateTimer);
+	}
+	else if (timer == backgroundMovingTimer)
+	{
+		backSceneObject.x -= backSceneObject.dx;
+		locateObject(backSceneObject.obj, backgroundScene, backSceneObject.x, backSceneObject.y);
+
+		if (backSceneObject.x < -3596)
+			backSceneObject.x = 1280;
+
+		setTimer(backgroundMovingTimer, 0.1f);
+		startTimer(backgroundMovingTimer);
+	}
+	else if (timer == enemy1GenTimer)
+	{
+		createEnemy(1);
+		setTimer(enemy1GenTimer, ENEMY_1_GEN_RATE);
+		startTimer(enemy1GenTimer);
+	}
+}
+
+
+void keyboardCallback(KeyCode kc, KeyState ks)
+{
+	if (gameStarted == false)
+		return;
+	if (ks == KeyState::KEYBOARD_PRESSED)
+	{
+		if (kc == 83)//오른족
+		{
+			heroAgent.dx = AGENT_SPEED;
+		}
+		else if (kc == 84)//위
+		{
+			heroAgent.dy = AGENT_SPEED;
+		}
+		else if (kc == 85) // 아래
+		{
+			heroAgent.dy = -AGENT_SPEED;
+		}
+		else if (kc == 82) //왼쪽
+		{
+			heroAgent.dx = -AGENT_SPEED;
+		}
+		else if (kc == 1) // a(공격)
+		{
+			createBulit(1);
+		}
+		setTimer(agentUpdateTimer, AGENT_UPDATE_TIME);
+		startTimer(agentUpdateTimer);
+	}
+	else if (ks == KeyState::KEYBOARD_RELEASED)
+	{
+		if (kc == 83)//오른족
+		{
+			if(heroAgent.dx == AGENT_SPEED)
+				heroAgent.dx=0;
+		}
+		else if (kc == 84)//위
+		{
+			if (heroAgent.dy == AGENT_SPEED)
+				heroAgent.dy = 0;
+		}
+		else if (kc == 85) // 아래
+		{
+			if (heroAgent.dy == -AGENT_SPEED)
+				heroAgent.dy = 0;
+		}
+		else if (kc == 82) //왼쪽
+		{
+			if (heroAgent.dx == -AGENT_SPEED)
+				heroAgent.dx = 0;
+		}
+		if (heroAgent.dx == 0 && heroAgent.dy == 0)
+			stopTimer(agentUpdateTimer);
+	}
+}
+
+/*
+utils
+*/
+void startGame()
+{
+	gameStarted = true;
+
+	//hide object
+	hideObject(startButtonObj);
+	hideObject(endButtonObj);
+
+	initObjects();
+	
+	//timers
+
+	/*
+	backgroundMovingTimer = createTimer(0.1f);
+	*/
+	initTimers();
+
+	startTimer(backgroundMovingTimer);
+	startTimer(enemy1GenTimer);
+	startTimer(objectUpdateTimer);
+	startTimer(agentUpdateTimer);
+
+}
+
+void endGame(bool success)
+{
+	stopTimer(agentUpdateTimer);
+	stopTimer(enemy1GenTimer);
+	stopTimer(backgroundMovingTimer);
+	stopTimer(objectUpdateTimer);
+
+	gameStarted = false;
+
+	setObjectImage(startButtonObj, "Images/restart.png");
+	showObject(startButtonObj);
+	showObject(endButtonObj);
+
+
+
+	locateObject(heroAgent.obj, backgroundScene, heroAgent.x, heroAgent.y);
+}
+
+ObjectID createObject(const char* image, SceneID scene, int x, int y, bool shown)
+{
+	ObjectID object = createObject(image);
+	locateObject(object, backgroundScene, x, y);
+
+	if (shown) {
+		showObject(object);
+	}
+
+	return object;
+}
+
+/*
+actions
+*/
+
+void agentMove(int dx, int dy)
+{
+	//x,y축 이동범위 제한
+	if (heroAgent.x + heroAgent.dx > SCREEN_WIDTH || heroAgent.x + heroAgent.dx < 0 
+		|| heroAgent.y + heroAgent.dy > SCREEN_HEIGHT-100 || heroAgent.y + heroAgent.dy < 0)
+	{
+		return;
+	}
+	heroAgent.x += heroAgent.dx;
+	heroAgent.y += heroAgent.dy;
+
+	locateObject(heroAgent.obj, backgroundScene, heroAgent.x, heroAgent.y);
+	if (heroAgent.x > 1280) {
+		endGame(true);
+	}
+	
+}
+
+void createEnemy(int type)
+{
+	enemyObject e;
+	int yPos = rand() % (SCREEN_HEIGHT - 100);
+	int dx = -((rand() % 10) + 2);//가로속도
+
+	if (type == 1)
+	{
+		e.x = SCREEN_WIDTH;
+		e.y = yPos;
+		e.dx = dx;
+		e.dy = 0;
+		e.hp = 30;
+		e.obj = createObject("Images/e1.png", backgroundScene, e.x, e.y, true);
+		enemyList1.push_back(e);
+	}
+	else if (type == 2)
+	{
+		//두번째 유형
+	}
+	else if (type == 3)
+	{
+		//세번째 유형
+	}
+}
+
+void createBulit(int type)
+{
+
+	bulitObject b;
+	int x = heroAgent.x+60;
+	int y = heroAgent.y+20;
+	int dx = 50;//탄속
+	int dy = 0;
+	if (type == 1)
+	{
+		b.x = x;
+		b.y = y;
+		b.dx = dx;
+		b.dy = dy;
+		b.damage = 10;
+		b.obj = createObject("Images/bulit.png", backgroundScene, b.x, b.y,true);
+		bulitList.push_back(b);
+	}
+	else if(type == 2)
+	{
+		//TBD 공격변형 지원
+
+	}
+}
+void initTimers()
+{
+
+	setTimer(backgroundMovingTimer, 0.3f);
+	setTimer(enemy1GenTimer, ENEMY_1_GEN_RATE);
+	setTimer(objectUpdateTimer, OBJECT_UPDATE_TIME);
+	setTimer(agentUpdateTimer, AGENT_UPDATE_TIME);
+
+}
+
+void initObjects()
+{
+	initEnemys();
+	initHeroAgent();
+	initEnemys();
+	initBackScene();
+}
+
+void initBackScene()
+{
+	backSceneObject.x = 0;
+	backSceneObject.y = -150;
+}
+
+
+void initHeroAgent()
+{
+	heroAgent.x = 0;
+	heroAgent.y = 500;
+	heroAgent.dx = 0;
+	heroAgent.dy = 0;
+}
+
+void initBulits()
+{
+	list<bulitObject>::iterator iterB;
+	for (iterB = bulitList.begin(); iterB != bulitList.end(); )
+	{
+		hideObject((*iterB).obj);
+		iterB = bulitList.erase(iterB);
+	}
+}
+void initEnemys()
+{
+	list<enemyObject>::iterator iterE;
+	for (iterE = enemyList1.begin(); iterE != enemyList1.end(); )
+	{
+		hideObject((*iterE).obj);
+		iterE = enemyList1.erase(iterE);
+	}
+}
+
+bool isAgentHit(int x, int y, int width, int height)
+{
+	return (heroAgent.x < x&& x < heroAgent.x + width
+		&& heroAgent.y < y&& y < heroAgent.y + height);
+}
+
+pair<bool,int> isBulitHit(int x, int y, int width, int height)
+{
+	list<bulitObject>::iterator iterB;
+	if(bulitList.empty())
+		return make_pair(false, 0);
+
+
+	for (iterB = bulitList.begin(); iterB != bulitList.end(); )
+	{
+		int damage = 0;
+		if ( x- 100< (*iterB).x && (*iterB).x < x + width + 100
+			&& y -100 < (*iterB).y && (*iterB).y < y + height + 100)
+		{
+			hideObject((*iterB).obj);
+
+			damage = (*iterB).damage;
+			iterB = bulitList.erase(iterB);
+			return make_pair(true, damage);
+		}
+		else
+			iterB++;
+	}
+	return make_pair(false,0);
+
+}
